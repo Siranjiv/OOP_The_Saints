d34146641f69801ddb061dc73ff6ee4fc5241a00
dravid29 <mugunthandravid11@gmail.com>
2020-05-05 21:42:02 +0530

    Models of class diagram

---
+++ OOP_Group_Project/src/main/java/Models/Employee.java
@@ -0,0 +1,10 @@
+
+package Models;
+
+public class Employee {
+    public String emp_id;
+    public String emp_name;
+    public int age;
+    private String position;
+    
+}
---
+++ OOP_Group_Project/src/main/java/Models/Work.java
@@ -0,0 +1,13 @@
+
+package Models;
+
+
+public class Work {
+    
+   public String emp_id;
+    
+    public void giveWork(String emp_id,String emp_name){
+
+    }
+    
+}
