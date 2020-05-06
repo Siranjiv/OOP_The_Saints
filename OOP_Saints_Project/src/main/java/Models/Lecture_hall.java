265b3cbc5183ec2de7459cb59bcb7e83a128a456
Siranjiv <siranjiv@gmail.com>
2020-05-06 10:02:43 +0530

    Model classes of undergraduate and postgraduate degree program

---
+++ OOP_Saints_Project/src/main/java/Models/Lecture_hall.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author WIN 10
+ */
+public class Lecture_hall {
+    
+    public String Hall_no;
+    public int table_no; 
+    public int chair_no;
+    
+    
+}
---
+++ OOP_Saints_Project/src/main/java/Models/Lecturer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author WIN 10
+ */
+public class Lecturer extends Person{
+    
+    private float Salary;
+    public String LID;
+    public String Position;
+    public String Qualiﬁcation; 
+    public String emailID; 
+    
+    
+    public void teach() 
+    {
+    
+    }
+            
+    public void Supervise() 
+    {
+    
+    }
+    
+    private float Mark_Exam() 
+    {
+    
+    }
+    private void Prepare_Examination()
+    {
+    
+    }
+
+    
+}
