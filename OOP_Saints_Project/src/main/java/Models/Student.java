3f33ce4d75a9b7410efc4096679fac0df3170cd3
Siranjiv <siranjiv@gmail.com>
2020-05-06 10:34:53 +0530

    Model classes of undergraduate and postgraduate degree program

---
+++ OOP_Saints_Project/src/main/java/Models/Student.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models;
+
+/**
+ *
+ * @author WIN 10
+ */
+public class Student extends Person{
+    
+    public String SID;
+    public String degree;
+    public String emailID;
+    
+                            
+      public void Write_exam()
+      {
+      
+      }
+      public void Projects()
+      {
+      
+      
+      }
+      
+              
+      public void Co_curricular_Activities()
+      {
+      
+      
+      }
+              
+      private double Checkresults()
+      {
+              
+              
+      }
+            
+      private double Check_GPA()
+      {
+            
+          
+      }
+                      
+}
