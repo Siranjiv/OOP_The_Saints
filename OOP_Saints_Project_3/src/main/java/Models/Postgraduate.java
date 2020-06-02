4cd497ef1ab50d6919c10a383de7ec07edd9f9df
Siranjiv <siranjiv@gmail.com>
2020-06-02 05:48:19 +0530

    Database is required to develop further but the overall idea is available

---
+++ OOP_Saints_Project_3/src/main/java/Models/Postgraduate.java
@@ -0,0 +1,17 @@
+
+package Models;
+
+
+public class Postgraduate extends Degree{
+    
+    
+    
+    
+    public Postgraduate(String c,String D,Float f,String in){
+    
+    super(c,D,f,in);
+    
+    }
+    
+    
+}
