9a18e8f42b0a3e82ff401003590785122e0ccd5c
Kurushoth <60101679+Kurushoth@users.noreply.github.com>
2020-06-02 12:02:05 +0530

    Using jTable to store data

---
+++ Enroll_Student/src/main/java/Regestation_Form/date.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Regestation_Form;
+
+/**
+ *
+ * @author KURUSHOTH.S
+ */
+class date {
+
+    static void setText(String format) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
